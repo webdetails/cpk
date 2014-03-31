1cf64140ecde5dea7d1df16deb7b0b733d747969
Guilherme Raimundo <guilherme.raimundo@webdetails.pt>
2014-03-31 11:45:59 +0100

    [SERVDEV-22] - Added pluginId to KettleResultKey. Changed elementName to elementId in KettleResultKey.

--- cpk-core/src/pt/webdetails/cpk/elements/impl/KettleResultKey.java
+++ cpk-core/src/pt/webdetails/cpk/elements/impl/KettleResultKey.java
@@ -1,32 +1,36 @@
 package pt.webdetails.cpk.elements.impl;
 
 import java.io.Serializable;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 
 public final class KettleResultKey implements Serializable {
-  private final HashMap<String, String> parameters;
+  private final String pluginId;
+  private final String elementId;
   private final String outputStepName;
-  private final String elementName;
+  private final Map<String, String> parameters;
 
-  public String getElementName() {
-    return this.elementName;
+  public String getPluginId() {
+    return this.pluginId;
   }
-
+  public String getElementId() {
+    return this.elementId;
+  }
   public String getOutputStepName() {
     return this.outputStepName;
   }
-
   public Map<String, String> getParameters() {
     return this.parameters;
   }
 
+
   // Constructors
 
-  public KettleResultKey( String elementName, String outputStepName, Map<String, String> parameters ) {
-    this.elementName = elementName;
+  public KettleResultKey( String pluginId, String elementId, String outputStepName, Map<String, String> parameters ) {
+    this.pluginId = pluginId;
+    this.elementId = elementId;
     this.outputStepName = outputStepName;
-    this.parameters = new HashMap<String, String>( parameters );
+    this.parameters = Collections.unmodifiableMap( parameters );
   }
 
   @Override
@@ -40,10 +44,10 @@ public final class KettleResultKey implements Serializable {
 
     final KettleResultKey that = (KettleResultKey) other;
 
-    return
-         stringEquals( this.elementName, that.elementName )
-      && stringEquals( this.outputStepName, that.outputStepName );
-
+    return stringEquals( this.pluginId, that.pluginId )
+      && stringEquals( this.elementId, that.elementId )
+      && stringEquals( this.outputStepName, that.outputStepName )
+      && this.parameters.equals( that.parameters );
   }
 
   private static boolean stringEquals( String textL, String testR ) {
@@ -52,7 +56,8 @@ public final class KettleResultKey implements Serializable {
 
   @Override
   public int hashCode() {
-    int result = stringHashCode( this.elementName );
+    int result = stringHashCode( this.pluginId );
+    result = 31 * result + stringHashCode( this.elementId );
     result = 31 * result + stringHashCode( this.outputStepName );
     result = 31 * result + this.parameters.hashCode();
     return result;
