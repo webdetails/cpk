71fcc660d96bbb71626a693121a1a6499f4adb8d
David Alexandre Marques dos Santos <david.santos@hitachivantara.com>
2018-07-17 19:04:47 +0100

    [CLEANUP] Code cleanup

--- core/src/main/java/pt/webdetails/cpk/CpkEngine.java
+++ core/src/main/java/pt/webdetails/cpk/CpkEngine.java
@@ -1,5 +1,5 @@
 /*!
-* Copyright 2002 - 2017 Webdetails, a Hitachi Vantara company.  All rights reserved.
+* Copyright 2002 - 2018 Webdetails, a Hitachi Vantara company.  All rights reserved.
 *
 * This software was developed by Webdetails and is provided under the terms
 * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -216,7 +216,7 @@ public class CpkEngine {
 
   /**
    * A comma separated list of allowed domains for CORS
-   * @return a comma separated list of doamins if the setting is present, null or an empty string otherwise
+   * @return a comma separated list of domains if the setting is present, null or an empty string otherwise
    */
   public String getDomainWhitelist() {
     return domainWhitelist;
