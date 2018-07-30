d1e43badb69705fded2606a4e971300bd1b806cb
Marco Vala <mvala@pentaho.com>
2018-07-30 10:57:02 +0100

    Revert "[BACKLOG-24379] Pentaho App Builder is not working in 8.1 GA"

--- pentaho5/src/main/java/pt/webdetails/cpk/InterPluginBroker.java
+++ pentaho5/src/main/java/pt/webdetails/cpk/InterPluginBroker.java
@@ -1,5 +1,5 @@
 /*!
-* Copyright 2002 - 2018 Webdetails, a Hitachi Vantara company.  All rights reserved.
+* Copyright 2002 - 2017 Webdetails, a Hitachi Vantara company.  All rights reserved.
 *
 * This software was developed by Webdetails and is provided under the terms
 * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -10,90 +10,34 @@
 * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
 * the license for the specific language governing your rights and limitations.
 */
+
 package pt.webdetails.cpk;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.commons.io.IOUtils;
 import pt.webdetails.cpf.PluginEnvironment;
-import pt.webdetails.cpf.PluginSettings;
 import pt.webdetails.cpf.plugin.CorePlugin;
 import pt.webdetails.cpf.plugincall.api.IPluginCall;
 import pt.webdetails.cpf.plugincall.base.CallParameters;
-import pt.webdetails.cpf.utils.PluginIOUtils;
 
 import java.io.OutputStream;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
 
+
 public class InterPluginBroker {
-  private static Log logger = LogFactory.getLog( InterPluginBroker.class );
-
-  static final String CDE_RENDER_API_BEAN_ID_TAG = "cde-render-api-bean-id";
-
-  static final String CDE_RENDER_API_BEAN_ID = "renderApi";
-  static final String CDE_RENDER_API_LEGACY_BEAN_ID = "renderer";
-
-  static final String CDE_RENDER_API_RENDER_METHOD_TAG = "cde-render-api-render-method";
-  static final String CDE_RENDER_API_RENDER_METHOD = "render";
-
-  private static IPluginCall cdeRenderApiCall;
 
   public static void run( Map<String, Object> params, OutputStream out ) throws Exception {
-    IPluginCall cdeRenderer = getCdeRenderer();
-    if ( cdeRenderer == null ) {
-      logger.error( "No InterPluginCall found for CDE Renderer." );
-
-      return;
-    }
-
     CallParameters parameters = new CallParameters();
-    for ( String key : params.keySet() ) {
-      String value = params.get( key ).toString();
-
-      parameters.put( key, value );
+    Iterator<String> it = params.keySet().iterator();
+    while ( it.hasNext() ) {
+      String key = it.next();
+      Object value = params.get( key );
+      parameters.put( key, value.toString() );
     }
-
-    String response = cdeRenderer.call( parameters.getParameters() );
-    PluginIOUtils.writeOutAndFlush( out, response );
-  }
-
-  private static IPluginCall getCdeRenderer() {
-    if ( cdeRenderApiCall != null ) {
-      return cdeRenderApiCall;
-    }
-
-    final String pluginID = CorePlugin.CDE.getId();
-    final PluginEnvironment environment = PluginEnvironment.env();
-
-    // 1. try configured values
-    String beanId = getSettingValue( CDE_RENDER_API_BEAN_ID_TAG, "" );
-    String method = getSettingValue( CDE_RENDER_API_RENDER_METHOD_TAG, "" );
-
-    IPluginCall cdePluginCall = environment.getPluginCall( pluginID, beanId, method );
-    if ( cdePluginCall.exists() ) {
-      return ( cdeRenderApiCall = cdePluginCall );
-    }
-
-    // 2. fallback to latest cde render bean id
-    cdePluginCall = environment.getPluginCall( pluginID, CDE_RENDER_API_BEAN_ID, CDE_RENDER_API_RENDER_METHOD );
-    if ( cdePluginCall.exists() ) {
-      return ( cdeRenderApiCall = cdePluginCall );
-    }
-
-    // 3. fallback to legacy cde render bean id
-    cdePluginCall = environment.getPluginCall( pluginID, CDE_RENDER_API_LEGACY_BEAN_ID, CDE_RENDER_API_RENDER_METHOD );
-    if ( cdePluginCall.exists() ) {
-      return ( cdeRenderApiCall = cdePluginCall );
-    }
-
-    return null;
-  }
-
-  private static String getSettingValue( String tag, String defaultValue ) {
-    final PluginSettings settings = PluginEnvironment.env().getPluginSettings();
-
-    List<String> values = settings.getTagValue( tag );
-    return values.isEmpty() ? defaultValue : values.get( 0 );
+    IPluginCall pluginCall = PluginEnvironment.env().getPluginCall( CorePlugin.CDE.getId(), "renderer", "render" );
+    String returnVal = pluginCall.call( parameters.getParameters() );
+    IOUtils.write( returnVal, out );
+    out.flush();
   }
 
 }
